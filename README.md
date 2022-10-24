0d52857faf2580ccb7022cebc340256f576d7433
anniesch <annieboulder@gmail.com>
2022-10-24 13:09:38 -0700

    Cheetah envs

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# single-life-rl
+# Single-Life Reinforcement Learning
+
+This code supplements the following paper: 
+
+> [You Only Live Once: Single-Life Reinforcement Learning](https://arxiv.org/pdf/2107.09044.pdf)
+
+As a starting point, the cheetah environments are provided in `envs/` with the corresponding prior data in `data/`.
+
+
+## Instructions
+
+Install [MuJoCo](http://www.mujoco.org/) from [here](https://github.com/openai/mujoco-py).
+
+Install the following libraries:
+```sh
+sudo apt update
+sudo apt install libosmesa6-dev libgl1-mesa-glx libglfw3
+```
+
+## Citation
+
+```
+@article{chen2022you,
+  title={You Only Live Once: Single-Life Reinforcement Learning},
+  author={Chen, Annie S and Sharma, Archit and Levine, Sergey and Finn, Chelsea},
+  journal={Neural Information Processing Systems},
+  year={2022}
+}
+```
---
+++ envs/half_cheetah_short.py
@@ -0,0 +1,61 @@
+import numpy as np
+from gym import utils
+# from gym.envs.mujoco import mujoco_env
+from . import mujoco_env
+from PIL import Image
+
+# Cheetah with short torso, can learn to flip upright
+# Has resets every self.max_steps steps
+class HalfCheetahEnvShort(mujoco_env.MujocoEnv, utils.EzPickle):
+    def __init__(self):
+        print("INIT SHORT HALF CHEETAH")
+        self.max_steps = 500
+        self.cur_steps = 0
+        mujoco_env.MujocoEnv.__init__(self, "half_cheetah_short.xml", 5)
+        utils.EzPickle.__init__(self)
+        
+    def step(self, action):
+        xposbefore = self.sim.data.qpos[0]
+        self.do_simulation(action, self.frame_skip)
+        xposafter = self.sim.data.qpos[0]
+        ob = self._get_obs()
+        reward_ctrl = -0.1 * np.square(action).sum()
+        reward_run = (xposafter - xposbefore) #/ self.dt
+        reward = reward_ctrl + reward_run / self.dt
+        rotation = self.sim.data.qpos[2]
+        reward=(reward_run/self.dt)+reward_ctrl - np.abs(self.sim.data.qpos[2] - 0)
+        done = self.is_successful(ob)
+        if not done and self.cur_steps >= self.max_steps:
+            done = True
+        self.cur_steps += 1
+    
+        return ob, reward, done, dict(reward_run=reward_run, total_reward=reward)
+
+    def _get_obs(self):
+        return np.concatenate(
+            [
+                self.sim.data.qpos.flat[1:],
+                self.sim.data.qvel.flat,
+                [-1]
+            ]
+        ).reshape(-1)
+
+    def reset_model(self):
+        self.cur_steps = 0
+        qpos = self.init_qpos + self.np_random.uniform(
+            low=-0.1, high=0.1, size=self.model.nq
+        )
+        qvel = self.init_qvel + self.np_random.standard_normal(self.model.nv) * 0.1
+        self.set_state(qpos, qvel)
+        return self._get_obs()
+
+    def viewer_setup(self):
+        self.viewer.cam.distance = self.model.stat.extent * 0.5
+        
+    def save_im(self, im, name):
+        img = Image.fromarray(im.astype(np.uint8)) 
+        img.save(name)
+        
+    def is_successful(self, obs=None):
+        x_pos2 =self.get_body_com('bfoot')[0]
+        return x_pos2 > 300
